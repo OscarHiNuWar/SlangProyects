8d14b3b000d525e0b93eb448a27ea172eece5f9b
Oscar Enrique Marchena <oscarhinuwar@gmail.com>
2020-01-17 17:03:09 -0400

    test

---
+++ PruebaGeneral/ContentPublished.cs
@@ -0,0 +1,117 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services.Implement;
+
+
+namespace WebApplication14.EventsSubscribers
+{
+    // register our component with Umbraco using a Composer
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class ContentPublishedComposer : ComponentComposer<ContentPublishedComponent>
+    {
+        // nothing needed to be done here!
+    }
+
+    public class ContentPublishedComponent : IComponent
+    {
+        // initialize: runs once when Umbraco starts
+        public void Initialize()
+        {
+            // subscribe to content service published event
+            ContentService.Published += ContentService_Published;
+        }
+
+        private void ContentService_Published(Umbraco.Core.Services.IContentService sender, Umbraco.Core.Events.ContentPublishedEventArgs e)
+        {
+            var Content = e.PublishedEntities.First();
+            if (Content.ContentType.Alias == "categoria")
+            {
+                var excel = Content.Properties["importarExcel"].GetValue();
+                if (excel!=null)
+                {
+                    var Excel = Path.Combine("http://localhost:65295" + excel.ToString());
+                    var client = new WebClient();
+                    var fullPath = Path.GetTempFileName();
+                    client.DownloadFile(Excel, fullPath);
+                    ObtenerEstudiantesMeritorios(fullPath, Content,e);
+                }
+            }
+        }
+
+
+        private void ObtenerEstudiantesMeritorios(string fileName, IContent Content, ContentPublishedEventArgs e)
+        {
+            long records = 0;
+            int ChildCount = Current.Services.ContentService.CountChildren(Content.Id);
+            if (ChildCount > 0)
+            {
+                var Childrens = Current.Services.ContentService.GetPagedChildren(Content.Id, 0, ChildCount, out records);
+                foreach (var child in Childrens)
+                {
+                    Current.Services.ContentService.Delete(child);
+                }
+            }
+
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = doc.WorkbookPart;
+                SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                SharedStringTable sst = sstpart.SharedStringTable;
+
+                var sheets = workbookPart.Workbook.Sheets.Cast<Sheet>().ToList();
+                var worksheetParts = workbookPart.WorksheetParts;
+                worksheetParts = worksheetParts.OrderBy(w => sheets.IndexOf(sheets.Where(x => x.Id.Value == doc.WorkbookPart.GetIdOfPart(w)).First()));
+                foreach (var worksheetPart in worksheetParts)
+                {
+                    Worksheet sheet = worksheetPart.Worksheet;
+                    var rows = sheet.Descendants<Row>().Skip(1);
+
+                    foreach (Row row in rows)
+                    {
+                        var celdas = row.Elements<Cell>().ToArray();
+                        if (celdas[0].CellValue != null)
+                        {
+                            var Numero = celdas[0].CellValue.Text;
+                            var Matricula = celdas[1].CellValue.Text;
+                            var NombreCompleto = sst.ChildElements[int.Parse(celdas[2].CellValue.Text)].InnerText;
+                            var Decanato = sst.ChildElements[int.Parse(celdas[3].CellValue.Text)].InnerText;
+                            var Carrera = sst.ChildElements[int.Parse(celdas[4].CellValue.Text)].InnerText;
+                            var CodigoCarrera = sst.ChildElements[int.Parse(celdas[5].CellValue.Text)].InnerText;
+                            var Estudiante = Current.Services.ContentService.Create($"{Matricula} - {NombreCompleto}", Content.Id, "estudianteMeritorio");
+                            Estudiante.Properties["no"].SetValue(Numero);
+                            Estudiante.Properties["matricula"].SetValue(Matricula);
+                            Estudiante.Properties["nombreCompleto"].SetValue(NombreCompleto);
+                            Estudiante.Properties["decanato"].SetValue(Decanato);
+                            Estudiante.Properties["carrera"].SetValue(Carrera);
+                            Estudiante.Properties["codigoCarrera"].SetValue(CodigoCarrera);
+                            Current.Services.ContentService.SaveAndPublish(Estudiante,"*", -1,false);
+                            
+                        }
+                        else
+                            break;
+
+                    }
+
+
+
+                }
+            }
+        }
+
+        // terminate: runs once when Umbraco stops
+        public void Terminate()
+        {
+            // do something when Umbraco terminates
+        }
+    }
+}
---
+++ PruebaGeneral/Funciones/ExcelToUmbraco.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Core.Services.Implement;
+using Umbraco.Core.Models;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Events;
+using System.IO;
+using System.Net;
+
+namespace PruebaGeneral.Funciones
+{
+
+    
+    public class ExcelToUmbraco : IComponent
+    {
+        private readonly ILogger _logger;
+        public IContentService _prueba;
+
+        [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+        public class LogWhenPublishedComposer : ComponentComposer<ExcelToUmbraco> { }
+
+        public ExcelToUmbraco(ILogger logger, IContentService prueba)
+        {
+            _logger = logger;
+            _prueba = prueba; 
+        }
+
+        // initialize: runs once when Umbraco starts
+        public void Initialize()
+        {
+            // subscribe to content service published event
+            ContentService.Published += ContentService_Published;
+        }
+
+        public void ReadExcelFileSax(string fileName, IContent e)
+        {
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+
+                SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                SharedStringTable sst = sstpart.SharedStringTable;
+
+                var sheets = workbookPart.Workbook.Sheets.Cast<Sheet>().ToList();
+                var worksheetParts = workbookPart.WorksheetParts;
+                worksheetParts = worksheetParts.OrderBy(w => sheets.IndexOf(sheets.Where(x => x.Id.Value == spreadsheetDocument.WorkbookPart.GetIdOfPart(w)).First()));
+
+                OpenXmlReader reader = OpenXmlReader.Create(workbookPart);
+
+                string text = string.Empty;
+                Worksheet sheet = worksheetPart.Worksheet;
+                var rows = sheet.Descendants<Row>().Skip(1);
+
+                foreach (Row row in rows)
+                {
+                    var celdas = row.Elements<Cell>().ToArray();
+                    if (celdas[0].CellValue != null)
+                    {
+                        var Nombre = sst.ChildElements[int.Parse(celdas[1].CellValue.Text)].InnerText;
+                        var Descripcion = sst.ChildElements[int.Parse(celdas[2].CellValue.Text)].InnerText; 
+                        var Sinonimos = sst.ChildElements[int.Parse(celdas[1].CellValue.Text)].InnerText;
+                        //var Fecha = DateTime.Parse(sst.ChildElements[int.Parse(celdas[1].CellValue.Text)].InnerText);
+                        var content = _prueba.Create(Nombre, e.Id, "slangs");
+                        content.SetValue("descripcion", Descripcion);
+                        content.SetValue("sinonimos", Sinonimos);
+                        //content.SetValue("fechaPublicacion", Fecha);
+                        _prueba.SaveAndPublish(content, "*", -1, false);
+                    }
+                    else { break; }
+                }
+
+                /*while (reader.Read())
+                {
+                    text = reader.GetText();
+                    var content = _prueba.Create("My First Blog Post 2", e.Id, "slangs");
+
+                    content.SetValue("descripcion", text);
+                    _prueba.SaveAndPublish(content, "*", -1, false);
+                }*/
+
+            }
+        }
+
+        private void ContentService_Published(IContentService sender, ContentPublishedEventArgs e)
+        {
+            // the custom code to fire everytime content is published goes here!
+            var ide = e.PublishedEntities.First();
+            if (ide.ContentType.Alias == "contenedorSlang")
+            {
+                var xls = ide.GetValue("Documento");
+                //var excel = Content.Properties["Documento"].GetValue();
+                if(xls.ToString() != String.Empty || xls != null)
+                {
+                    var docExcel = Path.Combine("https://localhost:44347" + xls.ToString());
+                    var cliente = new WebClient();
+                    var fullPath = Path.GetTempFileName();
+                    cliente.DownloadFile(docExcel, fullPath);
+                    ReadExcelFileSax(fullPath, ide);
+                }
+                
+                //var content = _prueba.Create("My First Blog Post 2", ide, "slangs");
+                //content.SetValue("descripcion", "<p>Test</p>");
+                //_prueba.SaveAndPublish(content, "*", -1, false);
+            }
+
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
